9088cb5d9bde64c0bc0c4eaae7a700cec25c8185
Abel Osaretin <contact.abel321@gmail.com>
2025-04-30 20:59:46 +0100

    end of part 3

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config()
+const express = require('express')
 // const mongoose = require("mongoose");
-const Note = require("./models/note");
+const Note = require('./models/note')
 
-const app = express();
+const app = express()
 
 // DO NOT SAVE YOUR PASSWORD TO GITHUB!!
 // const password = process.argv[2];
@@ -46,31 +46,31 @@ const app = express();
 // ];
 
 const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
-app.use(express.static("dist"));
-app.use(express.json());
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+app.use(express.static('dist'))
+app.use(express.json())
 
-app.use(requestLogger);
+app.use(requestLogger)
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
 
 // app.get("/api/notes", (request, response) => {
 //   response.json(notes);
 // });
 
-app.get("/api/notes", (request, response) => {
+app.get('/api/notes', (request, response) => {
   Note.find({}).then((notes) => {
-    console.log(notes);
-    response.json(notes);
-  });
-});
+    console.log(notes)
+    response.json(notes)
+  })
+})
 
 // app.get("/api/notes/:id", (request, response) => {
 //   const id = request.params.id;
@@ -83,27 +83,27 @@ app.get("/api/notes", (request, response) => {
 //   }
 // });
 
-app.get("/api/notes/:id", (request, response, next) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
-        response.json(note);
+        response.json(note)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
     // .catch((error) => {
     //   console.log(error);
     //   response.status(400).send({ error: "malformatted id" });
     // });
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-};
+// const generateId = () => {
+//   const maxId =
+//     notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+//   return String(maxId + 1);
+// };
 
 // app.post("/api/notes", (request, response) => {
 //   const body = request.body;
@@ -125,75 +125,75 @@ const generateId = () => {
 //   response.json(note);
 // });
 
-app.post("/api/notes", (request, response, next) => {
-  const body = request.body;
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ error: "content missing" });
+    return response.status(400).json({ error: 'content missing' })
   }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-  });
+  })
 
   note
     .save()
     .then((savedNote) => {
-      response.json(savedNote);
+      response.json(savedNote)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/notes/:id", (request, response, next) => {
-  const { content, important } = request.body;
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
 
   Note.findById(request.params.id)
     .then((note) => {
       if (!note) {
-        return response.status(404).end();
+        return response.status(404).end()
       }
 
-      note.content = content;
-      note.important = important;
+      note.content = content
+      note.important = important
 
       return note.save().then((updatedNote) => {
-        response.json(updatedNote);
-      });
+        response.json(updatedNote)
+      })
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/notes/:id", (request, response, next) => {
+app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
-    .then((result) => {
-      response.status(204).end();
+    .then(() => {
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
 // this has to be the last loaded middleware, also all the routes should be registered before this!
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
