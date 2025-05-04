3049a3131da9a4e37746cb936025e8197bf68f7d
Abel Osaretin <contact.abel321@gmail.com>
2025-05-04 13:26:49 +0100

    course info

--- mongo.js
+++ mongo.js
@@ -1,34 +1,34 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
 const url = `mongodb+srv://contactabel321:${password}@cluster0.gl6brir.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-});
+})
 
-const Note = mongoose.model("Note", noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 
 const newDBNote = new Note({
-  content: "Hi, Happy to Learn Saving to DB",
+  content: 'Hi, Happy to Learn Saving to DB',
   important: true,
-});
+})
 
 newDBNote.save().then((result) => {
-  console.log("note saved!", result);
-  mongoose.connection.close();
-});
+  console.log('note saved!', result)
+  mongoose.connection.close()
+})
 
 // Note.find({}).then((result) => {
 //   result.forEach((note) => {
